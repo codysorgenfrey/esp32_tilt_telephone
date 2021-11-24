73cd2ae0b07789a1a375118dedc0baec6ab41cd9
Cody Sorgenfrey <cosorgen@microsoft.com>
2021-11-23 16:42:27 -0800

    Working on v0.1

--- tilt-telephone-esp32.ino
+++ tilt-telephone-esp32.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <WiFi.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
@@ -8,115 +8,44 @@
 #include <BLEEddystoneTLM.h>
 #include <BLEBeacon.h>
 
-#define ENDIAN_CHANGE_U16(x) ((((x)&0xFF00) >> 8) + (((x)&0xFF) << 8))
+#include "secrets.h"
 
-int scanTime = 5; //In seconds
-BLEScan *pBLEScan;
+#define ENDIAN_CHANGE_U16(x) ((((x)&0xFF00) >> 8) + (((x)&0xFF) << 8))
+#define uS_TO_M_FACTOR 60000000ULL
+
+int loopInterval = 1; // In minutes
+int bleScanTime = 5; //In seconds
+
+class Tilt {
+    public: 
+        char *color;
+        double gravity;
+        double temp;
+};
+
+Tilt curTilt;
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 {
     void onResult(BLEAdvertisedDevice advertisedDevice)
     {
-        if (advertisedDevice.haveName())
+        if (advertisedDevice.haveManufacturerData() == true)
         {
-            Serial.print("Device name: ");
-            Serial.println(advertisedDevice.getName().c_str());
-            Serial.println("");
-        }
+            std::string strManufacturerData = advertisedDevice.getManufacturerData();
 
-        if (advertisedDevice.haveServiceUUID())
-        {
-            BLEUUID devUUID = advertisedDevice.getServiceUUID();
-            Serial.print("Found ServiceUUID: ");
-            Serial.println(devUUID.toString().c_str());
-            Serial.println("");
-        }
-        else
-        {
-            if (advertisedDevice.haveManufacturerData() == true)
+            uint8_t cManufacturerData[100];
+            strManufacturerData.copy((char *)cManufacturerData, strManufacturerData.length(), 0);
+
+            if (strManufacturerData.length() == 25 && cManufacturerData[0] == 0x4C && cManufacturerData[1] == 0x00)
             {
-                std::string strManufacturerData = advertisedDevice.getManufacturerData();
-
-                uint8_t cManufacturerData[100];
-                strManufacturerData.copy((char *)cManufacturerData, strManufacturerData.length(), 0);
-
-                if (strManufacturerData.length() == 25 && cManufacturerData[0] == 0x4C && cManufacturerData[1] == 0x00)
-                {
-                    Serial.println("Found an iBeacon!");
-                    BLEBeacon oBeacon = BLEBeacon();
-                    oBeacon.setData(strManufacturerData);
-                    Serial.printf("iBeacon Frame\n");
-                    Serial.printf("ID: %04X Major: %d Minor: %d UUID: %s Power: %d\n", oBeacon.getManufacturerId(), ENDIAN_CHANGE_U16(oBeacon.getMajor()), ENDIAN_CHANGE_U16(oBeacon.getMinor()), oBeacon.getProximityUUID().toString().c_str(), oBeacon.getSignalPower());
-                }
-                else
-                {
-                    Serial.println("Found another manufacturers beacon!");
-                    Serial.printf("strManufacturerData: %d ", strManufacturerData.length());
-                    for (int i = 0; i < strManufacturerData.length(); i++)
-                    {
-                        Serial.printf("[%X]", cManufacturerData[i]);
-                    }
-                    Serial.printf("\n");
-                }
-            }
-            return;
-        }
-
-        uint8_t *payLoad = advertisedDevice.getPayload();
-
-        BLEUUID checkUrlUUID = (uint16_t)0xfeaa;
-
-        if (advertisedDevice.getServiceUUID().equals(checkUrlUUID))
-        {
-            if (payLoad[11] == 0x10)
-            {
-                Serial.println("Found an EddystoneURL beacon!");
-                BLEEddystoneURL foundEddyURL = BLEEddystoneURL();
-                std::string eddyContent((char *)&payLoad[11]); // incomplete EddystoneURL struct!
-
-                foundEddyURL.setData(eddyContent);
-                std::string bareURL = foundEddyURL.getURL();
-                if (bareURL[0] == 0x00)
-                {
-                    size_t payLoadLen = advertisedDevice.getPayloadLength();
-                    Serial.println("DATA-->");
-                    for (int idx = 0; idx < payLoadLen; idx++)
-                    {
-                        Serial.printf("0x%08X ", payLoad[idx]);
-                    }
-                    Serial.println("\nInvalid Data");
-                    return;
-                }
-
-                Serial.printf("Found URL: %s\n", foundEddyURL.getURL().c_str());
-                Serial.printf("Decoded URL: %s\n", foundEddyURL.getDecodedURL().c_str());
-                Serial.printf("TX power %d\n", foundEddyURL.getPower());
-                Serial.println("\n");
-            }
-            else if (payLoad[11] == 0x20)
-            {
-                Serial.println("Found an EddystoneTLM beacon!");
-                BLEEddystoneTLM foundEddyURL = BLEEddystoneTLM();
-                std::string eddyContent((char *)&payLoad[11]); // incomplete EddystoneURL struct!
-
-                eddyContent = "01234567890123";
-
-                for (int idx = 0; idx < 14; idx++)
-                {
-                    eddyContent[idx] = payLoad[idx + 11];
-                }
-
-                foundEddyURL.setData(eddyContent);
-                Serial.printf("Reported battery voltage: %dmV\n", foundEddyURL.getVolt());
-                Serial.printf("Reported temperature from TLM class: %.2fC\n", (double)foundEddyURL.getTemp());
-                int temp = (int)payLoad[16] + (int)(payLoad[15] << 8);
-                float calcTemp = temp / 256.0f;
-                Serial.printf("Reported temperature from data: %.2fC\n", calcTemp);
-                Serial.printf("Reported advertise count: %d\n", foundEddyURL.getCount());
-                Serial.printf("Reported time since last reboot: %ds\n", foundEddyURL.getTime());
-                Serial.println("\n");
-                Serial.print(foundEddyURL.toString().c_str());
-                Serial.println("\n");
+                Serial.println("Found an iBeacon!");
+                BLEBeacon oBeacon = BLEBeacon();
+                oBeacon.setData(strManufacturerData);
+                Serial.printf("iBeacon Frame\n");
+                Serial.printf("ID: %04X Major: %d Minor: %d UUID: %s Power: %d\n", oBeacon.getManufacturerId(), ENDIAN_CHANGE_U16(oBeacon.getMajor()), ENDIAN_CHANGE_U16(oBeacon.getMinor()), oBeacon.getProximityUUID().toString().c_str(), oBeacon.getSignalPower());
+                curTilt.color = "Black";
+                curTilt.gravity = ENDIAN_CHANGE_U16(oBeacon.getMajor());
+                curTilt.temp = ENDIAN_CHANGE_U16(oBeacon.getMinor());
             }
         }
     }
@@ -125,23 +54,78 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Scanning...");
+    pinMode(LED_BUILTIN, OUTPUT); // set up onboard LED
 
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Scanning for Tilts...");
     BLEDevice::init("");
-    pBLEScan = BLEDevice::getScan(); //create new scan
+    BLEScan *pBLEScan = BLEDevice::getScan(); //create new scan
     pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
     pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
     pBLEScan->setInterval(100);
     pBLEScan->setWindow(99); // less or equal setInterval value
+    BLEScanResults foundDevices = pBLEScan->start(bleScanTime, false);
+    Serial.println("Tilts found: " + foundDevices.getCount());
+    digitalWrite(LED_BUILTIN, LOW);
+
+    if (foundDevices.getCount() > 0 && false) // THIS ISN'T SET UP YET
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("Connecting to " + String(ssid));
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+        }
+        Serial.println("");
+        Serial.println("IP address: " + WiFi.localIP());
+        
+        // Use WiFiClient class to create TCP connections
+        Serial.println("connecting to " + String(host));
+        WiFiClient client;
+        const int httpPort = 80;
+        if (!client.connect(host, httpPort)) {
+            Serial.println("connection failed");
+            return;
+        }
+
+        // We now create a URI for the request
+        String url = "/input/";
+        url += streamId;
+        url += "?private_key=";
+        url += privateKey;
+        url += "&value=";
+        url += "0";
+
+        // This will send the request to the server
+        client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                    "Host: " + host + "\r\n" +
+                    "Connection: close\r\n\r\n");
+        unsigned long timeout = millis();
+        while (client.available() == 0) {
+            if (millis() - timeout > 5000) {
+                Serial.println(">>> Client Timeout !");
+                client.stop();
+                return;
+            }
+        }
+
+        // Read all the lines of the reply from server and print them to Serial
+        while(client.available()) {
+            String line = client.readStringUntil('\r');
+            Serial.print(line);
+        }
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    pBLEScan->clearResults(); // delete results fromBLEScan buffer to release memory
+
+    esp_sleep_enable_timer_wakeup(loopInterval * uS_TO_M_FACTOR);
+    Serial.flush(); 
+    esp_deep_sleep_start();
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
-    BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-    Serial.print("Devices found: ");
-    Serial.println(foundDevices.getCount());
-    Serial.println("Scan done!");
-    pBLEScan->clearResults(); // delete results fromBLEScan buffer to release memory
-    delay(2000);
+    // no loop with deep sleep
 }
